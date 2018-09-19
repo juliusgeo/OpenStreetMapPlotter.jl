5bfb6dcbe312eb3dd549e4ac4368fd51e5e2583e
Julius Park <juliusgeo@gmail.com>
2018-09-19 10:26:26 -0400

    added relation parsing

--- src/OpenStreetMapParser2.jl
+++ src/OpenStreetMapParser2.jl
@@ -26,6 +26,18 @@ mutable struct Way
     Way() = new()
 end
 
+mutable struct Relation
+    id::String
+    visible::Bool
+    version::Int
+    changeset::String
+    timestamp::String
+    user::String
+    uid::String
+    members::Array{Any}
+    tags::Array{Tag}
+    Relation() = new()
+end
 
 function open_file(filepath::String)
 	xdoc = parse_file(filepath)
@@ -45,13 +57,19 @@ function parse_nodes(xroot::XMLElement)
 end
 
 function find_node(id::String, node_arr::Array{Node})
-    return node_arr[findfirst(x -> x.id == "37021436", node_arr)]
+    return node_arr[findfirst(x -> x.id == id, node_arr)]
 end
-
+function find_way(id::String, way_arr::Array{Way})
+	try
+    	return way_arr[findfirst(x -> x.id == id, way_arr)]
+    catch
+    	return false
+    end
+end
 function parse_ways(xroot::XMLElement)
-    way_arr = []
+    way_arr = Way[]
     node_arr = parse_nodes(xroot)
-    for way in xroot["way"]  # c is an instance of XMLNode
+    for way in xroot["way"]
         cur_way = Way()
         cur_way.id = attribute(way, "id")
         cur_way.visible = lowercase(attribute(way, "visible")) == "true"
@@ -73,6 +91,42 @@ function parse_ways(xroot::XMLElement)
     return way_arr
 end
 
-export open_file, parse_nodes, parse_ways, Node, Tag, Way
+function parse_relations(xroot::XMLElement, way_arr::Array{Way}, node_arr::Array{Node})
+	rel_arr = []
+    for rel in xroot["relation"]
+        cur_rel = Relation()
+        cur_rel.id = attribute(rel, "id")
+        cur_rel.visible = lowercase(attribute(rel, "visible")) == "true"
+        cur_rel.version = parse(Int, attribute(rel, "version"))
+        cur_rel.changeset = attribute(rel, "changeset")
+        cur_rel.timestamp = attribute(rel, "timestamp")
+        cur_rel.user = attribute(rel, "user")
+        cur_rel.uid = attribute(rel, "uid")
+        cur_rel.members = []
+        for member in rel["member"]
+        	ref = attribute(member, "ref")
+        	if attribute(member, "type") == "node"
+            	push!(cur_rel.members, find_node(ref, node_arr))
+            elseif attribute(member, "type") == "way"
+            	try
+            		temp_way = find_way(ref, way_arr)
+            		if temp_way != false
+            			push!(cur_rel.members, temp_way)
+            		end
+            	catch
+            		continue
+            	end
+            end
+        end
+        cur_rel.tags = []
+        for tag in rel["tag"]
+            push!(cur_rel.tags, Tag(attribute(tag, "k"), attribute(tag, "v")))
+        end
+        push!(rel_arr, cur_rel)
+    end
+    return rel_arr
+end
+
+export open_file, parse_nodes, parse_ways, parse_relations, Node, Tag, Way
 
 end
