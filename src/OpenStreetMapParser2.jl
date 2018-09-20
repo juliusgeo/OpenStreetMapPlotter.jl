c5791fa047402e4997be70ef4d0627a43ea09876
Julius Park <juliusgeo@gmail.com>
2018-09-20 19:19:51 -0400

    adding more styles

--- src/OpenStreetMapParser2.jl
+++ src/OpenStreetMapParser2.jl
@@ -3,6 +3,8 @@ using LightXML
 using HTTP
 using Winston
 
+include("dicts.jl")
+
 struct Node
     x::Float64
     y::Float64
@@ -62,7 +64,7 @@ function parse_nodes(xroot::XMLElement)
         id = attribute(node, "id")
         lat = parse(Float64, attribute(node, "lat"))
         lon = parse(Float64, attribute(node, "lon"))
-        push!(node_arr, Node(lat, lon, id))
+        push!(node_arr, Node(lon, lat, id))
     end
     return node_arr
 end
@@ -138,21 +140,33 @@ function parse_relations(xroot::XMLElement, way_arr::Array{Way}, node_arr::Array
     return rel_arr
 end
 
-function plot_ways(way_arr::Array{Way})
+function plot_ways(way_arr::Array{Way}, bbox::Tuple; width::Int64=500, roads_only::Bool=false)
+	minlon = bbox[1]
+	maxlon = bbox[3]
+	minlat = bbox[2]
+	maxlat = bbox[4]
+	c_adj = cosd((minlat + maxlat) / 2)
+    range_y = maxlat - minlat
+    range_x = maxlon - minlon
+	aspect_ratio = range_x * c_adj / range_y
+
+	fignum = Winston.figure(name="OpenStreetMap Plot", width=width, height=round(Int, width/aspect_ratio))
+	p = FramedPlot()
 	for way in way_arr
 		style=get_way_style(way.tags)
-    	p = plot([i.x for i in way.nodes], [i.y for i in way.nodes], style.spec, color=style.color, linewidth=style.width)
-    	hold(true)
-    	display(p)
+    	plot(p, [i.x for i in way.nodes], [i.y for i in way.nodes], style.spec, color=style.color, linewidth=style.width, xrange=(minlon, maxlon), yrange=(minlat, maxlat))
     end
-    hold(false)
+    display(p)
 end
-way_styles = Dict(
-	"motorway" => Style(0xe891a1, 3, "-"),
-	"trunk" => Style(0xf9b29c, 5, "-"),
-	"primary" => Style(0xFCD6A4, 5, "-"),
-	"secondary" => Style(0xF6F9BE, 4, "-"),
-	"tertiary" => Style(0xFEFEFE, 3, "-"),
+
+
+
+highway_styles = Dict(
+	"motorway" => Style(0x0015ff, 6, "-"), #bright blue
+	"trunk" => Style(0x9000ff, 5, "-"), #bright purple
+	"primary" => Style(0xff6868, 5, "-"), #orange
+	"secondary" => Style(0xfc4b4b, 4, "-"), #orange-ish red
+	"tertiary" => Style(0xfc3232, 3, "-"), #red
 	"unclassified" => Style(0xF1EEE8, 2, "-"),
 	"residential" => Style(0xF1EEE8, 2, "-"),
 	"service" => Style(0x007CFF, 1, "-"),
@@ -162,25 +176,66 @@ way_styles = Dict(
 	"secondary_link" => Style(0xF6F9BE, 2, "-"),
 	"tertiary_link" => Style(0xFEFEFE, 2, "-"),
 	"living_street" => Style(0xF1EEE8, 2, "-"),
-	"pedestrian" => Style(0x007CFF, 2, "-"),
-	"track" => Style(0x007CFF, 1, ":"),
-	"bus_guideway" => Style(0x007CFF, 1, ":"),
-	"escape" => Style(0x007CFF, 1, ":"),
-	"raceway" => Style(0x007CFF, 1, ":"),
-	"road" => Style(0x007CFF, 1, ":"),
-	"footway" => Style(0x007CFF, 1, ":"),
-	"bridleway" => Style(0x007CFF, 1, ":"),
-	"steps" => Style(0x007CFF, 1, ":"),
-	"path" => Style(0x007CFF, 1, ":")
+	"pedestrian" => Style(0x888888, 2, "-"),
+	"track" => Style(0x909090, 1, ":"),
+	"bus_guideway" => Style(0x989898, 1, ":"),
+	"escape" => Style(0xA0A0A0, 1, ":"),
+	"raceway" => Style(0xA8A8A8, 1, ":"),
+	"road" => Style(0xA9A9A9, 1, ":"),
+	"footway" => Style(0xB0B0B0, 1, ":"),
+	"bridleway" => Style(0xB8B8B8, 1, ":"),
+	"steps" => Style(0xBEBEBE, 1, ":"),
+	"path" => Style(0xC0C0C0, 1, ":")
+)
+building_styles = Dict(
+	"yes" => Style(0xe891a1, 1, "-"),
+	"house" => Style(0x91f2ce, 1, "-"),
+	"residential" => Style(0x91f2ce, 1, "-"),
+	"garage" => Style(0x91f2ce, 1, "-"),
+	"apartment" => Style(0x91f2ce, 1, "-"), #all residential stuff is greenish
+	"hut" => Style(0xe891a1, 1, "-"),
+	"industrial" => Style(0x876d6c, 1, "-"),
+	"detached" => Style(0xe891a1, 1, "-"),
+	"shed" => Style(0x876d6c, 1, "-"),
+	"commercial" => Style(0xfcb5b3, 1, "-"),
+	"terrace" => Style(0xe891a1, 1, "-"),
+	"garages" => Style(0xe891a1, 1, "-"),
+	"school" => Style(0xb2effc, 1, "-"), #schools are blue
+	"construction" => Style(0xf2d2d8, 1, "-"),
+)
+waterway_styles = Dict(
+	"stream" => Style(0x609af7, 3, "-"),
+	"ditch" => Style(0x609af7, 1, "-"),
+	"river" => Style(0x609af7, 5, "-"),
+	"drain" => Style(0x609af7, 1, "-"),
+	"riverbank" => Style(0x609af7, 1, "-"),
+	"canal" => Style(0x609af7, 1, "-"),
+	"other" => Style(0x609af7, 1, "-"),
+)
+leisure_styles = Dict(
+	"pitch" => Style(0x30bc0d, 2, "-"),
+	"swimming_pool" => Style(0x609af7, 2, "-"),
+	"park" => Style(0x30bc0d, 2, "-"),
+	"playground" => Style(0x30bc0d, 1, "-"),
+	"garden" => Style(0x30bc0d, 1, "-"),
+	"sports_centre" => Style(0x30bc0d, 1, "-"),
+	"other" => Style(0x30bc0d, 1, "-"),
+)
+tag2style = Dict(
+	"waterway" => waterway_styles,
+	"building" => building_styles,
+	"highway" => highway_styles,
+	"leisure" => leisure_styles,
 )
 function get_way_style(tags::Dict)
-	if haskey(tags, "highway")
-		if haskey(way_styles, tags["highway"])
-			#println(tags["highway"])
-			return way_styles[tags["highway"]]
+	for tag in ["waterway", "building", "highway", "leisure"]
+		if haskey(tags, tag)
+			if haskey(tag2style[tag], tags[tag])
+				return tag2style[tag][tags[tag]]
+			end
 		end
 	end
-	return Style(0x000000, 1, "-")
+	return Style(0x808080, 1, "-")
 end
 export open_file, open_bbox, parse_nodes, parse_ways, parse_relations, plot_ways, Node, Tag, Way
 
---
+++ src/dicts.jl
@@ -0,0 +1,53 @@
+
+highway_styles = Dict(
+	"motorway" => Style(0xe891a1, 3, "-"),
+	"trunk" => Style(0xf9b29c, 5, "-"),
+	"primary" => Style(0xFCD6A4, 5, "-"),
+	"secondary" => Style(0xF6F9BE, 4, "-"),
+	"tertiary" => Style(0xFEFEFE, 3, "-"),
+	"unclassified" => Style(0xF1EEE8, 2, "-"),
+	"residential" => Style(0xF1EEE8, 2, "-"),
+	"service" => Style(0x007CFF, 1, "-"),
+	"motorway_link" => Style(0xe891a1, 2, "-"),
+	"trunk_link" => Style(0xf9b29c, 2, "-"),
+	"primary_link" => Style(0xFCD6A4, 2, "-"),
+	"secondary_link" => Style(0xF6F9BE, 2, "-"),
+	"tertiary_link" => Style(0xFEFEFE, 2, "-"),
+	"living_street" => Style(0xF1EEE8, 2, "-"),
+	"pedestrian" => Style(0x888888, 2, "-"),
+	"track" => Style(0x909090, 1, ":"),
+	"bus_guideway" => Style(0x989898, 1, ":"),
+	"escape" => Style(0xA0A0A0, 1, ":"),
+	"raceway" => Style(0xA8A8A8, 1, ":"),
+	"road" => Style(0xA9A9A9, 1, ":"),
+	"footway" => Style(0xB0B0B0, 1, ":"),
+	"bridleway" => Style(0xB8B8B8, 1, ":"),
+	"steps" => Style(0xBEBEBE, 1, ":"),
+	"path" => Style(0xC0C0C0, 1, ":")
+)
+building_styles = Dict(
+	"yes" => Style(0xe891a1, 1, "-"),
+	"house" => Style(0x91f2ce, 1, "-"),
+	"residential" => Style(0x91f2ce, 1, "-"),
+	"garage" => Style(0x91f2ce, 1, "-"),
+	"apartment" => Style(0x91f2ce, 1, "-"), #all residential stuff is greenish
+	"hut" => Style(0xe891a1, 1, "-"),
+	"industrial" => Style(0xe891a1, 1, "-"),
+	"detached" => Style(0xe891a1, 1, "-"),
+	"shed" => Style(0xe891a1, 1, "-"),
+	"commercial" => Style(0xe891a1, 1, "-"),
+	"terrace" => Style(0xe891a1, 1, "-"),
+	"garages" => Style(0xe891a1, 1, "-"),
+	"school" => Style(0x857aff, 1, "-"), #schools are blue
+	"construction" => Style(0xe891a1, 1, "-"),
+)
+waterway_styles = Dict(
+	"stream" => Style(0x609af7, 1, "-"),
+	"ditch" => Style(0x609af7, 1, "-"),
+	"river" => Style(0x609af7, 1, "-"),
+	"drain" => Style(0x609af7, 1, "-"),
+	"riverbank" => Style(0x609af7, 1, "-"), #all residential stuff is greenish
+	"canal" => Style(0x609af7, 1, "-"),
+	"other" => Style(0x609af7, 1, "-"),
+)
+
