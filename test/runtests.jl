406760f5b911a9bdc6133ba11d4ec20e87713ad2
Julius Park <juliusgeo@gmail.com>
2018-11-08 11:44:40 -0500

    tests working now

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,22 @@
  
 #Start Test Script
 using OpenStreetMapParser2
-import Base.Test
- 
+import Test
+using Pkg
 # Run tests
  
-println("Test xml parsing")
-#@time @test_throws parse_ways("blahblah.osm")
-@time @test way_arr, bbox = parse_ways(open_file("west_philly.osm"))
-@time @test way_arr.nodes == Node[Node(-75.1758, 39.956, "109728966"), Node(-75.1757, 39.9564, "3513769053"), Node(-75.1756, 39.9565, "109728979")]
-@time @test way_arr.tags == Dict{Any,Any}("name"=>"Schuylkill River","waterway"=>"river")
-@time savejson(way_arr, "test.geojson") 
+println("Testing xml parsing...")
+way_arr, bbox = parse_ways(open_file("west_philly.osm"))
+test_nodes = way_arr[1].nodes[1:10]
+Test.@test test_nodes[1].id == "27149008"
+Test.@test test_nodes[2].id == "588195318"
+Test.@test test_nodes[3].id == "27149007"
+Test.@test test_nodes[4].id == "27149006"
+Test.@test test_nodes[5].id == "27149005"
+Test.@test test_nodes[6].id == "1288458928"
+Test.@test test_nodes[7].id == "27149004"
+Test.@test test_nodes[8].id == "27149003"
+Test.@test test_nodes[9].id == "1288458923"
+Test.@test test_nodes[10].id == "27149002"
+Test.@test way_arr[1].tags == Dict{Any,Any}("name"=>"Schuylkill River","waterway"=>"river")
+Test.@time save_json(way_arr, "test.geojson") 
